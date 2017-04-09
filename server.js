7716c9d5a86e795e318169f364e5905c0c23be8f
Chandan Kumar <chn291289@gmail.com>
2017-04-09 23:00:12 +0530

    stable commit

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+var http = require('http');
+var fs = require('fs');
 var mongo = require('mongodb').MongoClient;
 var client = require('socket.io').listen(8080).sockets;
 
-mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
+function onRequest(req, res) {
+    res.writeHead('200', { 'Content-Type': 'text/html' });
+
+    fs.readFile('./index.html', function(err, data) {
+        if (err) {
+            console.log(err);
+            res.writeHead(404);
+            res.write('Page Not Found');
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
+}
+
+http.createServer(onRequest).listen(8888);
+
+mongo.connect('mongodb://127.0.0.2/chat', function(err, db) {
     if (err) throw err;
     client.on('connection', function(socket) {
         console.log('someone connected');
@@ -12,12 +31,19 @@ mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
             var name = data.name;
             var message = data.message;
 
+            //emit latest message to all clients
+            client.emit('output', [data]);
+
             coll.insert({ name: name, message: message }, function() {
                 console.log('inserted');
             });
         });
 
-        //emit status to client
-        socket.emit('output', );
+        //emit all messages to client
+        var allMsg = coll.find().limit(100).sort({ _id: 1 }).toArray(function(err, res) {
+            if (err) throw err;
+            socket.emit('output', res);
+        });
+
     })
-})
+});
